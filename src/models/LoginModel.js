17efd3b040938104426c4d4b18b070d0e83b11d4
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-08-09 14:18:58 -0300

    Funcionalidade de login de usuário completa

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -17,6 +17,22 @@ class Login {
         this.user = null;
     }
 
+    async login(){
+        this.valida();
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({email: this.body.email});
+
+        if (!this.user){
+            this.errors.push('Usuário não existe!');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha inválida');
+            this.user = null;
+        }
+    }
+
     async register(){
         // Registrar o usuario
         this.valida();
@@ -29,17 +45,14 @@ class Login {
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
-        }
+        this.user = await LoginModel.create(this.body);
+        
     }
 
     async userExists(){
         // Método para checar se o usuário já está cadastradp na base de dados
-        const user = await LoginModel.findOne({email: this.body.email});
-        if (user) this.errors.push('Usuário já cadastrado');
+        this.user = await LoginModel.findOne({email: this.body.email});
+        if (this.user) this.errors.push('Usuário já cadastrado');
     }
 
     valida(){
